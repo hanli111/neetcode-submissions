d2c42abdf7a6dcf2692ea30ccf8c4e60a65602b8
Han Li <139891189+hanli111@users.noreply.github.com>
2026-06-06 12:39:00 -0400

    Add: two-integer-sum-ii - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-14.py
@@ -0,0 +1,48 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # l = 0
+        # r = 0
+        # while numbers[l] + numbers[r] != target:
+        #     r += 1
+        #     if r == len(numbers):
+        #         r = 0
+        #         l += 1
+        # return [min(l + 1, r + 1), max(l + 1, r + 1)]
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            cur_sum = numbers[l] + numbers[r]
+            if cur_sum > target:
+                r -= 1
+            elif cur_sum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
+        return []
+
+        '''
+         0  1  2  3
+        [1, 2, 3, 4]  target = 3 -> return [1, 2] since 1-indexed
+        
+            R
+
+         0   1   2   3
+        [12, 13, 54, 58]  target = 112 -> return [3, 4]
+                     L
+                 R
+
+         0  1  2
+        [2, 3, 4]   target = 6 -> return [1, 3]
+         L
+               R
+    
+        l = 0
+        r = 0
+        while nums[l] + nums[r] != target
+            r += 1
+            if r == len(nums)
+                r = 0
+                l += 1
+        return [min(l + 1, r + 1), max(l + 1, r + 1)]
+
+        
+        '''
