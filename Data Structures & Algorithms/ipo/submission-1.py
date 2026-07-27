9e608dbc3ef5ca8a9ad0b22f0e6d52be3f922522
Han Li <139891189+hanli111@users.noreply.github.com>
2026-07-27 14:10:37 -0400

    Add: ipo - submission-1

---
+++ Data Structures & Algorithms/ipo/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        # store (capital, profit)
+        min_heap = [(c, p) for c, p in zip(capital, profits)]
+        heapq.heapify(min_heap)
+        max_heap = []
+        
+        while k > 0:
+            while min_heap and w >= min_heap[0][0]:
+                c, p = heapq.heappop(min_heap)
+                heapq.heappush_max(max_heap, p)
+            
+            if not max_heap:
+                break
+            
+            w += heapq.heappop_max(max_heap)
+            k -= 1
+        return w
