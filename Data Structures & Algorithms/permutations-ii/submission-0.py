71215eaf10d03d366ca46f2912ae59b77932fbbf
Han Li <139891189+hanli111@users.noreply.github.com>
2026-07-31 09:14:02 -0400

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def backtrack(perm, pick):
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+            
+            for i in range(len(nums)):
+                if pick[i]:
+                    continue
+                    
+                if i > 0 and nums[i] == nums[i - 1] and not pick[i - 1]:
+                    continue
+                
+                perm.append(nums[i])
+                pick[i] = True
+                backtrack(perm, pick)
+                perm.pop()
+                pick[i] = False
+
+        backtrack([], [False] * len(nums))
+        return res
